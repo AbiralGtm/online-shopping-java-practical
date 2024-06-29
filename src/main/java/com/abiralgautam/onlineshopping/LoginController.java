51055fb7a4eb2e618ee3c3e9f9e31029b261ef8a
abiralgtm <abiralgautam19@gmail.com>
2024-06-29 14:27:42 +0545

    login servlet and connection with UI

--- src/main/java/com/abiralgautam/onlineshopping/LoginController.java
+++ src/main/java/com/abiralgautam/onlineshopping/LoginController.java
@@ -3,10 +3,16 @@ package com.abiralgautam.onlineshopping;
 import com.abiralgautam.onlineshopping.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginController {
 
@@ -15,13 +21,61 @@ public class LoginController {
 
     @FXML
     private PasswordField password;
+
+    @FXML
+    private Label responseLabel;
+
     @FXML
     private void login()
     {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setContentText("Username"+ username.getText() + "pas"+password.getText());
-        alert.showAndWait();
+        String username = this.username.getText();
+        String password = this.password.getText();
 
+        try{
+            String response = sendLoginPost("http://localhost:8080/onlineshopping/auth/login",username,password);
+            if(response.equals("login_successful")){
+                App.setRoot("home");
+            }else{
+                responseLabel.setText(response);
+                responseLabel.setStyle("-fx-text-fill: red");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            responseLabel.setText("Error while logging in");
+        }
+
+    }
+
+    private String sendLoginPost(String url, String username, String password) throws Exception{
+        URL loginUrl = new URL(url);
+
+        HttpURLConnection connection = (HttpURLConnection) loginUrl.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+        connection.setDoOutput(true);
+
+        String postData = "username=" + username+ "&password="+password;
+
+        try(OutputStream os = connection.getOutputStream()){
+            os.write(postData.getBytes());
+            os.flush();
+        }
+
+        int responseCode = connection.getResponseCode();
+
+        if(responseCode == HttpURLConnection.HTTP_OK) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                return response.toString();
+            }
+        }else{
+            return "Failed: HTTP error code:" + responseCode;
+        }
     }
 
     @FXML
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.abiralgautam.onlineshopping {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.servlet;
+    requires java.sql;
     opens com.abiralgautam.onlineshopping to javafx.fxml;
     exports com.abiralgautam.onlineshopping;
 }
